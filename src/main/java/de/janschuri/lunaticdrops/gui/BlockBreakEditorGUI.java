9ec43316165692362a9bc1cfa83192b12b9103d5
Janschuri <124630782+Janschuri@users.noreply.github.com>
2024-12-24 00:23:25 +0100

    display non item blocks

--- src/main/java/de/janschuri/lunaticdrops/gui/BlockBreakEditorGUI.java
+++ src/main/java/de/janschuri/lunaticdrops/gui/BlockBreakEditorGUI.java
@@ -10,7 +10,9 @@ import de.janschuri.lunaticlib.platform.bukkit.inventorygui.GUIManager;
 import de.janschuri.lunaticlib.platform.bukkit.inventorygui.InventoryButton;
 import de.janschuri.lunaticlib.platform.bukkit.inventorygui.SelectBlockGUI;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -60,9 +62,21 @@ public class BlockBreakEditorGUI extends EditorGUI {
         meta.setDisplayName("§dNo block selected");
         itemStack.setItemMeta(meta);
 
-        ItemStack item =
-                getBlockType() != null ? new ItemStack(getBlockType()) :
-                        itemStack;
+        if (getBlockType() != null) {
+            Material block = getBlockType();
+            if (!block.isItem()) {
+                itemStack = new ItemStack(Material.BARRIER);
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                itemMeta.setDisplayName("§r" + block.name());
+                itemMeta.addEnchant(Enchantment.MENDING, 1, true);
+                itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                itemStack.setItemMeta(itemMeta);
+            } else {
+                itemStack = new ItemStack(block);
+            }
+        }
+
+        ItemStack item = itemStack;
 
         return new InventoryButton()
                 .creator((player) -> item)
